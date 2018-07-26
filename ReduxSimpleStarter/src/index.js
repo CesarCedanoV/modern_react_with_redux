75cfd02c9e235824368587d35b7eb98c67c16a40
CesarCedanoV <CesarCedanoV@gmail.com>
2018-07-26 10:31:24 -0400

    Created the SearchBar component and Import it on the index.js to render it on the container

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './componets/search_bar';
 
 const YOUTUBE_API_KEY = "AIzaSyDHRVNJwicsA2qu1G1QXXFcoE-Qq61DhKQ"
 
 // Create a new component. This component should produce some HTML
 const App = () => {
-    return <div>Hello World! =)</div>;
-}
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />,document.querySelector('.container'));
