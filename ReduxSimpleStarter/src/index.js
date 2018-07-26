e75130f7ef7a3f2e7f92a4b65b3b02e24c23b4c2
CesarCedanoV <CesarCedanoV@gmail.com>
2018-07-26 13:17:57 -0400

    Refactored the App from a Functional Component to a Class Component

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './componets/search_bar';
-
 const YOUTUBE_API_KEY = "AIzaSyDHRVNJwicsA2qu1G1QXXFcoE-Qq61DhKQ"
 
 // Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
-};
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { 
+            videos: []
+        }
+        
+        YTSearch(
+            {
+                key:YOUTUBE_API_KEY,
+                term:'surfboards'
+            }, 
+            (videos) => this.setState({videos})
+        );
+
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
+}
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />,document.querySelector('.container'));
