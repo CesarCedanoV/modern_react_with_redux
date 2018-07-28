fb0d6296d5584c4e61f05ca14f1dc1eb2f3688be
CesarCedanoV <CesarCedanoV@gmail.com>
2018-07-28 18:22:31 -0400

    Created our first reducer named books that will return a list of books.

---
+++ booksApp/src/reducers/books.js
@@ -0,0 +1,16 @@
+export default function() {
+    return [
+        {
+            title:"JavaScript: The Good Parts"
+        },
+        {
+            title:"El Principito"
+        },
+        {
+            title:"The Alchemist "
+        },
+        {
+            title:"Astrophysics for People in a Hurry"
+        }
+    ]
+}
