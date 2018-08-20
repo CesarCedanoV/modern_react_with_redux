f499c7a92750837610f5c176b50ec64a63739231
Cesar A. Cedano V <ccedanovelez@crestviewc.com>
2018-08-20 15:25:40 -0400

    Added the GoogleMaps Components and also add some css to it

--- weatherApp/src/containers/weather_list.js
+++ weatherApp/src/containers/weather_list.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
-
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
     renderCityWeather(cityData){
@@ -9,9 +9,10 @@ class WeatherList extends Component {
         const cityTemperatures = cityData.list.map(weather => weather.main.temp);
         const cityPressures = cityData.list.map(weather => weather.main.pressure);
         const cityHumidities = cityData.list.map(weather => weather.main.humidity);
+        const {lon, lat} = cityData.city.coord;
         return(
             <tr key={cityName}>
-                <td>{cityName}</td>
+                <td><GoogleMap lon={lon} lat={lat}/></td>
                 <td><Chart data={cityTemperatures} color="red" units="Kelvin" /></td>
                 <td><Chart data={cityPressures} color="green" units="hPa" /></td>
                 <td><Chart data={cityHumidities} color="blue" units="%" /></td>
@@ -33,8 +34,8 @@ class WeatherList extends Component {
                 <tbody>
                         {this.props.weather.map(this.renderCityWeather)}
                 </tbody>
-           
-           </table> 
+
+           </table>
         );
     }
 }
