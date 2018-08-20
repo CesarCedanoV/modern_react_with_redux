6b5241c014ee2650fbbdef47aca03e28a5417094
CesarCedanoV <CesarCedanoV@gmail.com>
2018-08-20 07:28:04 -0400

    Added the Chart components and Pass the data comming from our Axios request.

---
+++ weatherApp/src/containers/weather_list.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart';
+
+
+class WeatherList extends Component {
+    renderCityWeather(cityData){
+        const cityName = cityData.city.name
+        const cityTemperatures = cityData.list.map(weather => weather.main.temp);
+        const cityPressures = cityData.list.map(weather => weather.main.pressure);
+        const cityHumidities = cityData.list.map(weather => weather.main.humidity);
+        return(
+            <tr key={cityName}>
+                <td>{cityName}</td>
+                <td><Chart data={cityTemperatures} color="red" units="Kelvin" /></td>
+                <td><Chart data={cityPressures} color="green" units="hPa" /></td>
+                <td><Chart data={cityHumidities} color="blue" units="%" /></td>
+            </tr>
+        );
+    }
+
+    render() {
+        return (
+           <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature (Kelvin) </th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                        {this.props.weather.map(this.renderCityWeather)}
+                </tbody>
+           
+           </table> 
+        );
+    }
+}
+
+function mapStateToProps({ weather }) {
+    return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
