6e8fe6fb8e7f8742081168cfc9e031ed6e2312f5
CesarCedanoV <CesarCedanoV@gmail.com>
2018-08-21 21:02:35 -0400

    Updated the blogApp folder and also Added the Router and Started the PostsList component

---
+++ blogApp/src/components/posts_list.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsList extends Component {
+    render() {
+        return(
+            <div>
+                PostsList
+            </div>
+        );
+    }
+}
+
+export default PostsList;
