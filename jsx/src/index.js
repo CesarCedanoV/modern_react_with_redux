c89965ed3728320b1a8221a867855d33a9b9795c
CesarCedanoV <CesarCedanoV@gmail.com>
2019-03-26 18:12:40 -0400

    Fixed a typo in the 'for' property

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const App = () => {
   return (
     <div>
-    <label classs="label" for="name"> Enter name:</label>
+    <label classs="label" htmlFor="name"> Enter name:</label>
     <input id="name" type="type"/>
     <button style={{backgroundColor:'blue', color:'white'}}>Submit</button>
   </div>
