b010353ea81265249b69cf4ad4cc14d6f4b727fd
Cesar A. Cedano V <ccedanovelez@crestviewc.com>
2019-04-04 09:12:35 -0400

    Created the Image List and also added some css to it.

---
+++ pics/src/components/ImageCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+  return() {
+    const {description, urls } = this.props.image;
+    return(
+      <div>
+        <img 
+          alt={description}
+          src={urls.regular}
+        />
+      </div>
+    );
+  }
+}
+
+export default ImageCard;
--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,11 +1,12 @@
+import './ImageList.css';
 import React from 'react';
-
+import ImageCard from './ImageCard';
 const ImageList = (props) => {
-  const images = props.images.map(({id, description, urls,}) => {
-    return <img key={id} alt={description} src={urls.thumb} />
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} image={image}  />;
   });
 
-  return <div>ImageList {images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList
