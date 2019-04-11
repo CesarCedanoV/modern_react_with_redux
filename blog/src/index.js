c777cd3b38403251c21c49ac3a20f0ad1743fca4
CesarCedanoV <CesarCedanoV@gmail.com>
2019-04-11 07:59:17 -0400

    To use Redux Thunk, i created a new blog proyect

---
+++ blog/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import App from './components/App'
+import reducers from './reducers';
+
+// To apply multiple store enhancers, we may use compose()
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducers,
+  composeEnhancer(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.querySelector('#root'));
