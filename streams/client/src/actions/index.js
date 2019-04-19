a0b691631c0ca9a9525f37541ef2a82aca8bb4f0
CesarCedanoV <CesarCedanoV@gmail.com>
2019-04-19 09:29:28 -0400

    Created a Modal Component that will be use in the StreamDelete component to pass some parameters to the Modal

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -55,4 +55,5 @@ export const deleteStream = streamId => async dispatch => {
   await streamsAPI.delete(`/streams/${streamId}`);
 
   dispatch({type:DELETE_STREAM, payload:streamId})
+  history.push('/');
 };
--- streams/client/src/components/streams/StreamDelete.js
+++ streams/client/src/components/streams/StreamDelete.js
@@ -1,7 +1,36 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import Modal from '../Modal';
+import history from '../../history';
+import { deleteStream } from '../../actions';
 
-const StreamDelete = () => {
-  return <div>StreamDelete</div>
-};
+class StreamDelete extends React.Component {
 
-export default StreamDelete;
+  renderActionBtns () {
+    return  (
+      <React.Fragment>
+        <button
+          onClick={() => this.props.deleteStream(this.props.match.params.id)}
+          className="ui negative button">Delete</button>
+        <button 
+          onClick={() => history.goBack()}
+            className="ui button">Cancel</button>
+      </React.Fragment>
+    )
+  }
+
+  render() {
+    return (
+      <Modal 
+        header="Delete stream"
+        content={`Are you sure you want to delete the Stream #${this.props.match.params.id}?`}
+        actionBtns={this.renderActionBtns()}
+        onDismiss={history.goBack}
+        />
+    );
+  } 
+}
+
+
+
+export default connect(null,{deleteStream})(StreamDelete);
