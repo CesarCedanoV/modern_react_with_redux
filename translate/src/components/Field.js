12e93983bae1cc0c615b876fceddef751f2aba0f
CesarCedanoV <CesarCedanoV@gmail.com>
2019-04-22 20:51:24 -0400

    Created a simple app to use the React Context API by defining a LanguageContext provider and some consumers

---
+++ translate/src/components/Field.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+
+class Field extends React.Component {
+  static contextType = LanguageContext;
+  render(){
+    return (
+      <div className="ui field">
+        <label>
+          <LanguageContext.Consumer>
+            {value => value.language === 'english' ? 'Name' : 'Nombre'}
+          </LanguageContext.Consumer>
+        </label>
+        <input />
+      </div>
+    );
+  }
+};
+
+export default Field;
